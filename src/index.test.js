ca54d185665ea63d5f06fc5db407f325bf546388
Dan Maxim <maxdanus@yahoo.com>
2017-10-02 12:00:47 -0400

    add mocha and jsdom

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,22 @@
 import {expect} from 'chai';
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', () => {
 	it('should pass', () => {
 		expect(true).to.equal(true);
+	});
+});
+
+
+describe('index.html', () => {
+	it('should say hello', (done) => {
+		const index = fs.readFileSync('./src/index.html', 'utf-8');
+		jsdom.env(index, function(err, window) {
+			const h2 = window.document.getElementsByTagName('h2')[0];
+			expect(h2.innerHTML).to.equal("Hello from the index page")
+			done();// Need to tell mocha test is done
+			window.close();
+		});
 	});
 });
